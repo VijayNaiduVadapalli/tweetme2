a0ad8aebfa20610a04e196373251070c4c9bcda2
viijaynaidu vadapalli <vijaynaidu440@mail.com>
2020-11-22 23:26:42 +0100

    Initial updated commit

--- tweets/views.py
+++ tweets/views.py
@@ -1,13 +1,29 @@
-from django.http import HttpResponse,Http404
-from django.shortcuts import render
+from django.http import HttpResponse,Http404,JsonResponse
+from django.shortcuts import render  
+from .models import Tweet
 
 def home_view(request,*args,**kwargs):
-     return HttpResponse("<div>hello world </div>")
+     return render(request,"pages/home.html",context={},status=200)
+def tweet_list_view(request,*args,**kwargs):
+    qs=Tweet.objects.all()
+    tweet_list=[{"id":x.id, "content":x.content}for x in qs]
+    data={
+      "response":tweet_list
+   }
+    return JsonResponse(data)
 def tweet_detail_view(request,tweet_id,*args,**kwargs):
-     try:
-        obj=Tweet.objects.get(id=tweet_id)
-     except:
-        raise Http404
-     return HttpResponse(f"<h1>hello {tweet_id} -{obj.content}</h1>")
+      """RestAPI view by javascript returns json data"""
+      data={
+      "isuser":False,
+       "id":tweet_id
+      }
+      status=200
+      try:
+         obj=Tweet.objects.get(id=tweet_id)
+         data['content']=obj.content
+      except:  
+         data['message']="Not Found"
+         status =404
+      return JsonResponse(data,status=status)
 
 
